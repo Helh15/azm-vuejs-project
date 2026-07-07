3ba19d13f9b3a899d5af280f08fb2610d361f685
HELH <loola.girl.1@gmail.com>
2026-07-07 06:25:00 +0300

    add  publicPath: process.env.NODE_ENV === 'production'   ? '/azm-vuejs-project/'   : '/'

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
    base: '/azm-vuejs-project/',
+    publicPath: process.env.NODE_ENV === 'production'
+  ? '/azm-vuejs-project/'
+  : '/'
   plugins: [
     vue(),
   ],
